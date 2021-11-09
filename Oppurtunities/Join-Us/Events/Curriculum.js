14310752288d3d4324924bd817e2a5b051a35bfe
chenj2031 <91810539+chenj2031@users.noreply.github.com>
2021-11-08 21:10:08 -0500

    Add files via upload

---
+++ Oppurtunities/Join-Us/Events/Curriculum.js
@@ -0,0 +1,51 @@
+import './App.css';
+import Curriculum from './Curriculum.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          
+          <h1> Curriculum
+
+          </h1>
+        </p>
+        <img className="Image-left" src={Curriculum} /> 
+
+          <h2> Description
+
+          </h2>
+          <p> 
+            Planning on teaching, mentoring or developing our curriculum?
+            
+          </p>
+          Apply to become Curriculum developer of TCF                        
+            You can comfortably communicate with students.                              
+            You have experience using programming languages (Java, Python, etc.)                             
+            You are self motivated player who is invented in teacher others about computer programming         
+            You are actively responding on Slack
+          
+
+          <h2> 
+          Responsibilities
+          </h2>
+          Regularly schedule meetings with the students.
+          <p> Perform Tasks in timely manner and consistently meet deadlines. </p>
+          Strong Communication, Public Speaking, and time management skills are a necessity.
+          Able to spend 2-3 hours/week.
+          <h2>
+            Requirements
+          </h2>
+          You don't have previous experience in this field? Don't worry! 
+          <p> We've got your back, you could still apply for this positions! </p>
+          You are required to have strong communication skills to communicate effectively with students. 
+&nbsp;
+        <button> Refer to a friend? </button> &nbsp;<button> Apply </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
